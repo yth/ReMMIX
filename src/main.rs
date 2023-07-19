0c8fe993a5a9f0c39dfd65402639efeb5088c18c
Yuan Cao <yuancao85@gmail.com>
2023-07-19 18:51:45 -0400

    basic test of using main memory

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,12 @@ fn main() {
     println!("{:?}", my_computer.special_purpose_registers.len());
     println!("{:?}", mmix::MEMORY_SIZE);
 
-    let slice = unsafe { std::slice::from_raw_parts_mut(my_computer.memory, mmix::MEMORY_SIZE as usize)};
+    {
+        let slice = unsafe { std::slice::from_raw_parts_mut(my_computer.memory, mmix::MEMORY_SIZE as usize)};
+        slice[100] = 1;
+    }
 
-    slice[100] = 1;
+    let slice = unsafe { std::slice::from_raw_parts_mut(my_computer.memory, mmix::MEMORY_SIZE as usize)};
     println!("{}", slice[99]);
     println!("{}", slice[100]);
     println!("{}", slice[101]);
